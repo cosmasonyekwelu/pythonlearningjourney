1b112ed390f98305e791e6a472cceea693690da7
Cosmas Onyekwelu <dpositiveworld@gmail.com>
2026-01-13 22:03:46 +0000

    ..

--- practice/mlh_intro.py
+++ practice/mlh_intro.py
@@ -3,11 +3,11 @@ import random
 
 # sample sentences 
 sentences = [ 
-    "The quick brown fox jumps over the lazy dog.", 
-    "Python is a great programming language.", 
-    "I love watching dominosaurs streams!", 
-    "Coding is fun and rewarding.", 
-    "Practice makes perfect." 
+    "The quick brown fox jumps over the lazy dog", 
+    "Python is a great programming language", 
+    "I love watching dominosaurs streams", 
+    "Coding is fun and rewarding", 
+    "Practice makes perfect" 
 ]
 
 # initial prompt 
